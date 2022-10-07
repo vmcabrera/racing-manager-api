dc2cc07796de468cb6267f4a99daff63b8a0ec4a
Victor Cabrera <vcabre92@gmail.com>
2022-10-07 15:38:55 +0200

    Implement JWT auth plugin

---
+++ src/api/plugins/jwt-auth.plugin.ts
@@ -0,0 +1,35 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import fp from 'fastify-plugin';
+import jwt from '@fastify/jwt';
+import { InternalError } from '../error/internal.error';
+
+export interface AuthPluginOptions {}
+
+export const jwtAuthPlugin = fp<AuthPluginOptions>(async (server, opts) => {
+  if (!process.env.JWT_KEY) throw new InternalError('Something went wrong');
+
+  server.register(jwt, {
+    secret: process.env.JWT_KEY,
+    sign: {
+      expiresIn: 1200,
+    },
+  });
+
+  server.decorate(
+    'jwtAuthenticate',
+    async function (req: FastifyRequest, res: FastifyReply, next: Function) {
+      try {
+        await req.jwtVerify();
+      } catch (err) {
+        res.send(err);
+      }
+      next();
+    }
+  );
+});
+
+declare module 'fastify' {
+  export interface FastifyInstance {
+    jwtAuthenticate(): string;
+  }
+}
